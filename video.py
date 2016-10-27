519238c81f1953a6f04dba5d86970eb8c8cd5a1e
maximus009 <kssrisingavenger@gmail.com>
2016-10-26 21:38:44 -0400

    generalized start and end time and frame dims

--- video.py
+++ video.py
@@ -3,15 +3,16 @@ import numpy as np
 import sys,os
 from glob import glob
 from config.resources import video_resource
+from config.global_parameters import frameWidth, frameHeight
 
-def get_frames(videoPath, time_step=2000):
+def get_frames(videoPath, start_time=5000, end_time=120000, time_step=2000):
 
     try:
         cap = cv2.VideoCapture(videoPath)
-        for k in range(5000, 120001, time_step):
+        for k in range(start_time, end_time+1, time_step):
             cap.set(cv2.cv.CV_CAP_PROP_POS_MSEC, k)
             success, frame = cap.read()
-            frame = cv2.resize(frame,(224,224))
+            frame = cv2.resize(frame,(frameWidth, frameHeight))
             yield frame
     except Exception as e:
         print e
