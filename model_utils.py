bc68f91d4df2be2c256b9932f30bd5ddcb071cf8
maximus009 <kssrisingavenger@gmail.com>
2016-10-27 21:08:02 -0400

    Removed last layers with speed up

--- model_utils.py
+++ model_utils.py
@@ -4,6 +4,20 @@ from keras import backend
 import numpy as np
 
 
+def remove_last_layers(model):
+    """To remove the last FC layers of VGG and get the 4096 dim features"""
+    model.layers.pop()
+    model.layers.pop()
+    model.outputs = [model.layers[-1].output]
+    model.layers[-1].outbound_nodes = []
+
+
+vgg_model_16 = VGG16(include_top=True, weights="imagenet")
+vgg_model_19 = VGG19(include_top=True, weights="imagenet")
+
+remove_last_layers(vgg_model_16)
+remove_last_layers(vgg_model_19)
+
 def get_features(image, model_name="vgg16"):
 
     if backend.image_dim_ordering()=='th':
@@ -11,17 +25,11 @@ def get_features(image, model_name="vgg16"):
         return None
 
     if model_name.lower() in ["vgg16", "vgg_16"]:
-        vgg_model_16 = VGG16(include_top=True, weights="imagenet")
         model = vgg_model_16
 
     if model_name.lower() in ["vgg19", "vgg_19"]:
-        vgg_model_19 = VGG19(include_top=True, weights="imagenet")
         model = vgg_model_19
 
-    model.layers.pop()
-    model.layers.pop()
-    model.outputs = [model.layers[-1].output]
-    model.layers[-1].outbound_nodes = []
     imageTensor = np.zeros((1, 224, 224, 3))
     imageTensor[0] = image
 
@@ -31,5 +39,5 @@ def get_features(image, model_name="vgg16"):
 
 if __name__=="__main__":
     import cv2
-    vector = get_features(cv2.resize(cv2.imread("testImages/test1.jpg"), (224,224)))
+    vector = get_features(cv2.resize(cv2.imread("testImages/test1.jpg"), (224,224)), 'vgg19')
     print vector.shape
