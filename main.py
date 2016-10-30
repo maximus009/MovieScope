98df0ff0042b18e2e32115c7fabaa7387e423ecb
maximus009 <kssrisingavenger@gmail.com>
2016-10-30 13:16:05 -0400

    Experiment. per frame. added callback for hualos visualization

--- main.py
+++ main.py
@@ -6,7 +6,11 @@ from config.global_parameters import frameWidth, frameHeight
 from config.resources import video_resource
 
 from video import extract_feature_video 
+from model_utils import spatial_model 
 
+"""testing hualos"""
+from keras import callbacks
+remote = callbacks.RemoteMonitor(root='http://localhost:9000')
 #collect videos for each genre
 #extract spatial features for each
 #create your model
@@ -19,7 +23,6 @@ def gather_videos(genre, limit_videos = 25):
     videoPaths = glob(video_resource+genre+'/*')
     videoFeatures = np.array([list(extract_feature_video(videoPath, verbose=True)) for videoPath in videoPaths[:limit_videos]])
     return videoFeatures
-
 
 
 def gather():
@@ -36,16 +39,44 @@ def gather():
     dump(romanceFeatures, open('data/romance.p','wb'))
     print "OK"
 
+
 def train():
     romanceFeatures = load_pkl('romance')
     horrorFeatures = load_pkl('horror')
-    for r in romanceFeatures:
-        print len(r),
-    print
-    for h in horrorFeatures:
-        print len(h)
+    romanceFeatures = np.array([np.array(f) for f in romanceFeatures])
+    horrorFeatures = np.array([np.array(f) for f in horrorFeatures])
+
+    model = spatial_model(2)
+    model.compile(optimizer='sgd', loss='binary_crossentropy', metrics=['accuracy'])
+
+    trainingData = []
+    trainingLabels = []
+    num_of_frames = 35
+    for i,videoFeatures in enumerate(romanceFeatures):
+        randomIndices = sorted(np.random.randint(0,len(videoFeatures),num_of_frames))
+        selectedFeatures = np.array(videoFeatures[randomIndices])
+        for feature in selectedFeatures:
+            trainingData.append(feature)
+            trainingLabels.append([1,0])
+
+    for i,videoFeatures in enumerate(horrorFeatures):
+        randomIndices = sorted(np.random.randint(0,len(videoFeatures),num_of_frames))
+        selectedFeatures = np.array(videoFeatures[randomIndices])
+        for feature in selectedFeatures:
+            trainingData.append(feature)
+            trainingLabels.append([0,1])
 
 
+    trainingData = np.array(trainingData)
+    trainingLabels = np.array(trainingLabels)
+    trainingLabels = trainingLabels.reshape((-1,2))
+    print trainingData.shape
+    print trainingData[0].shape
+    print trainingLabels.shape
+
+    model.fit(trainingData, trainingLabels, batch_size=16, nb_epoch=50, callbacks=[remote])
+
+    
 if __name__=="__main__":
     train()
 
