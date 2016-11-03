5c5bf45c4081164d6b22dbb9a480f6bf1b749d13
Gautam Somappa <Gautam Somappa>
2016-11-03 14:41:01 -0400

    Change path in config

--- config/resources.py
+++ config/resources.py
@@ -1,3 +1,3 @@
-video_resource = '../youtubeDL/' # This is where you keep your videos, inside each genre as a sub-folder
+video_resource = './genres/' # This is where you keep your videos, inside each genre as a sub-folder
 frame_resource = './data/' # Any resource/pickles can be stored here.
 vggWeightsPath = "/Users/sivaramanks/code/MovieScope/vgg16_weights.h5" # path to vgg weights if need be. (optional)
