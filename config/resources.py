622b19b01d028698446ad50d249993a451711a87
maximus009 <kssrisingavenger@gmail.com>
2016-11-07 15:41:12 -0500

    moved all models to specific folder

--- config/resources.py
+++ config/resources.py
@@ -1,2 +1,3 @@
 video_resource = '../youtubeDL/' # This is where you keep your videos, inside each genre as a sub-folder
 frame_resource = './data/' # Any resource/pickles can be stored here.
+model_resource = "./data/models/"
