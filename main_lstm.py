b7b307ebaed4fd58b34d71b1a749d31078ec7a9b
maximus009 <kssrisingavenger@gmail.com>
2016-11-10 09:54:32 -0500

    removing old function in lstm file

--- main_lstm.py
+++ main_lstm.py
@@ -35,56 +35,6 @@ def gather():
     gather_genre('romance',3)
 
 
-def _train_classifier(genres=['romance', 'horror', 'action'], num_of_videos=100):
-    
-    """Gather data for selected genres"""
-    trainingData = []
-    trainingLabels = []
-    num_of_random_frames = 35
-    num_of_classes = len(genres)
-    print "Number of classes:",num_of_classes
-    for genreIndex, genre in enumerate(genres):
-        print "Looking for pickle file: data/{0}{1}.p".format(genre, str(num_of_videos)),
-        try:
-            genreFeatures = load_pkl(genre+str(num_of_videos))
-            genreFeatures = np.array([np.array(f) for f in genreFeatures]) # numpy hack
-        except Exception as e:
-            print e
-            return
-        print "OK."
-        for videoFeatures in genreFeatures:
-            if len(videoFeatures) > num_of_random_frames:
-                randomIndices = np.random.randint(0, len(videoFeatures), num_of_random_frames)
-                selectedFeatures = np.array(videoFeatures[randomIndices])
-                for feature in selectedFeatures:
-                    trainingData.append(feature)
-                    trainingLabels.append([genreIndex])
-    trainingData = np.array(trainingData)
-    trainingLabels = np.array(trainingLabels)
-    print trainingData.shape 
-    print trainingLabels.shape
-#    trainingLabels = to_categorical(trainingLabels, num_of_classes)
-    print trainingLabels
-#    trainingLabels = trainingLabels.reshape((-1,num_of_classes))
-
-    """Initialize the model"""
-    """
-    model = lstm_model(num_of_classes)
-    model.compile(optimizer='sgd', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
-
-   
-    """
-    """Start training"""
-    """
-    batch_size = 64
-    nb_epoch = 5 
-
-    model.fit(trainingData, trainingLabels, batch_size=batch_size, nb_epoch=nb_epoch)#, callbacks=[remote])
-    modelOutPath = str(num_of_classes)+"g_bs"+str(batch_size)+"_ep"+str(nb_epoch)+"_nf_35.h5"
-#    model.save(modelOutPath)
-#    print "Model saved at",modelOutPath
-    
-    """
 def train_classifier(genres=['romance', 'horror', 'action'], num_of_videos=100):
     
     """Gather data for selected genres"""
