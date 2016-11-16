5604fd7062a72f722f7d0d060d1f75ab3c4875ad
maximus009 <kssrisingavenger@gmail.com>
2016-11-16 17:26:14 -0500

    Added actual MS dataset path

--- config/resources.py
+++ config/resources.py
@@ -1,3 +1,4 @@
 video_resource = '../youtubeDL/' # This is where you keep your videos, inside each genre as a sub-folder
+video_resource = './MovieScope_DataSet/'
 frame_resource = './data/' # Any resource/pickles can be stored here.
 model_resource = "./data/models/"
