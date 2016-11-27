694be8c0f3461f84e6544c16e03293d9da2a3c41
Gautam Somappa <Gautam Somappa>
2016-11-27 05:56:39 -0500

    Changed config file for local repo

--- config/resources.py
+++ config/resources.py
@@ -1,2 +1,2 @@
-video_resource = '../youtubeDL/' # This is where you keep your videos, inside each genre as a sub-folder
+video_resource = 'C:\Users\Admin\Downloads\Project\youtubeDL\dataset\\train' # This is where you keep your videos, inside each genre as a sub-folder
 frame_resource = './data/' # Any resource/pickles can be stored here.
